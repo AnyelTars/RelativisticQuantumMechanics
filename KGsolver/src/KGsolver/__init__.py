4c50b6272c2e851a9a0a4cb945c8cad5513013d4
AnyelTars <angel.salazar@yachaytech.edu.ec>
2026-02-05 15:40:48 -0500

    updated modules

--- KGsolver/src/KGsolver/__init__.py
+++ KGsolver/src/KGsolver/__init__.py
@@ -2,4 +2,4 @@ from .riccati import RiccatiModel
 from .potentials import expTanhPotential, TanhPotential, woodsSaxonPotential,cupsPotential
 from .spectrum import conservation_metrics
 
-__all__ = ["RiccatiModel", "expTanhPotential", "TanhPotential", "conservation_metrics"]
+__all__ = ["RiccatiModel", "expTanhPotential", "TanhPotential", "woodsSaxonPotential", "cupsPotential", "conservation_metrics"]
